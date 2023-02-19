259b3ca5849a4283f766519aca63e9152cc943ef
Orateng <1818.blackmail@gmail.com>
2023-02-19 07:23:50 -0800

    My size output for 32 and 64 bit machine

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-	printf("(Size of a cha: %lu bytes(s)\n", sizeof(char));
-	printf("(Size of a int: %lu bytes(s)\n", sizeof(int));
-	printf("(Size of a long int: %lu bytes(s)\n", sizeof(long));
-	printf("(Size of a long long: %lu bytes(s)\n", sizeof(long long));
-	printf("(Size of a float: %lu bytes(s)", sizeof(float));
+	printf("Size of a cha: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long));
+	printf("Size of a long long: %lu bytes(s)\n", sizeof(long long));
+	printf("Size of a float: %lu bytes(s)", sizeof(float));
 	return (0);
 }
