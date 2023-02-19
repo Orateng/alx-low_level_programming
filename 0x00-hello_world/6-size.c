91f986a2b716c68ab3963ebb86dc376256ba9af3
Orateng <1818.blackmail@gmail.com>
2023-02-19 08:05:09 -0800

    My sizes fir bit data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,20 @@
 
 int main(void)
 {
-	printf("Size of a cha: %lu bytes(s)\n", sizeof(char));
-	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(long));
-	printf("Size of a long long: %lu bytes(s)\n", sizeof(long long));
-	printf("Size of a float: %lu bytes(s)", sizeof(float));
+	char c;
+
+	int i;
+
+	long '&';
+
+	long long '^';
+
+	float d;
+
+	printf("Size of a char: %lu bytes(s)\n", size of(c));
+	printf("Size of a int: %lu bytes(s)\n", size of(i));
+	printf("Size of a long int: %lu bytes(s)\n", size of('&'));
+	printf("Size of a long long int: %lu bytes(s)\n", size of('^'));
+	printf("Size of a float: %lu bytes(s)", size of(d));
 	return (0);
 }
