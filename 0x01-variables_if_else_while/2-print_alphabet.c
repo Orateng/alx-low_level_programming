ae466095fe9d71c1917e453c359f5b2e01fa40a6
Orateng <1818.blackmail@gmail.com>
2023-02-19 16:31:36 -0800

    My alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Putchar function on main void.
+*
+*Return: Always 0.
+*/
+
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
+
+
