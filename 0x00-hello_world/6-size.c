b5d1c660f9223c2779eefbd412328ab37835af8e
Orateng <1818.blackmail@gmail.com>
2023-02-19 16:04:49 -0800

    My size data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,17 @@
 int main(void)
 {
 
-printf("Size of a char: %lu bytes(s)\n", sizeof(char));
-printf("(Size of an int: %lu bytes(s)\n", sizeof(int));
-printf("(Size of a long int: %lu bytes(s)\n", sizeof(long int));
-printf("(Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-printf("(Size of a float: %lu bytes(s)\n", sizeof(float));
+char charType;
+int intType;
+long int longType;
+long long int long_longType;
+float floatType;
+
+printf("Size of a char: %lu bytes(s)\n", sizeof(charType));
+printf("(Size of an int: %lu bytes(s)\n", sizeof(intType));
+printf("(Size of a long int: %lu bytes(s)\n", sizeof(longType));
+printf("(Size of a long long int: %lu bytes(s)\n", sizeof(long_longType));
+printf("(Size of a float: %lu bytes(s)\n", sizeof(floatType));
 
 return (0);
 
