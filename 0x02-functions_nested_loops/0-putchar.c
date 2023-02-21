edec230ded8ad6f97929d72107f0037cfa9e2683
Orateng <1818.blackmail@gmail.com>
2023-02-21 04:57:36 -0800

    My putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point
+* Description - Print _putchar as statndard output.
+* Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
