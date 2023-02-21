2f4fa42435ba035ebf967fd576aebe49b537d37b
Orateng <1818.blackmail@gmail.com>
2023-02-21 11:59:57 -0800

    My putchar make

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - Entry point
-* Description - Print _putchar as statndard output.
-* Return: always 0 (Success)
+* main - prit _putchar
+* is written by mbah
+* Return: 0 (Success)
 */
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
