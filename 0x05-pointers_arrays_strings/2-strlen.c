4289531ec02673818dcb958ddc094a9883296bf0
Orateng <1818.blackmail@gmail.com>
2023-02-27 12:07:40 -0800

    My string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - function to get the length of a string
+ * Description: get length of a string
+// * @*s: string length
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (; *s != '\0'; s++)
+	{
+		len += 1;
+	}
+	return (len);
+}
