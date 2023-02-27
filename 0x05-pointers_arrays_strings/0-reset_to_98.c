0d443e4c310fae118623b5c81bc01b03f9e7ac35
Orateng <1818.blackmail@gmail.com>
2023-02-27 09:20:56 -0800

    My repository update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _reset_to_98 - sets  reset value for pointer using parameter
+ * Description: reset pointer
+ * @n: description for parameter reset value
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
