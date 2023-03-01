ad3256be1a8696fe799f07761d50f996a65fc6d3
Orateng <1818.blackmail@gmail.com>
2023-03-01 08:36:21 -0800

    My reversed array content

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * reverse_array - compare values of string
+ * @a: array value
+ * @n: number of elements of array value
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
