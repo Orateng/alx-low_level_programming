93a3fb3ae36bc9e06366aa5836a3319e65c7a0d3
Orateng <1818.blackmail@gmail.com>
2023-03-02 12:06:55 -0800

    My number print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,7 +15,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		n1 = -n;
 	}
 
@@ -23,6 +23,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-		_putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
 
