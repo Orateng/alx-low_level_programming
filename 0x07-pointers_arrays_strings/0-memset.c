f642e47f0d773634d64154e5a6e03b4f2240c7c6
Orateng <1818.blackmail@gmail.com>
2023-03-06 09:56:54 -0800

    My hexadecimal print

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+ #include <string.h>
+/**
+* _memset - prints buffer in hexa
+* @s: starting address of memeory to be filled
+* @b: the desired value
+* @n: number of bytes to be changed
+*
+* Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
