b7dd4410d16af57956d645f6f3d3add1b006dc3b
Orateng <1818.blackmail@gmail.com>
2023-03-06 11:23:49 -0800

    My pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+* set_string - prints sum of  two diagonal square matrix integers
+* @s: pointer to pointer
+* @to: pointer char
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
