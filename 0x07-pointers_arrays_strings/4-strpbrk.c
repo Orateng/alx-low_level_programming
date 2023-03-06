05008e27cdf74d01f3ddf063116099581f92cb21
Orateng <1818.blackmail@gmail.com>
2023-03-06 10:36:58 -0800

    My search string print

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+
+#include "main.h"
+#include <string.h>
+/**
+* _strpbrk - string search
+* @s: input
+* @accept: input
+*
+* Return: always 0 (success)
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+{
+	for (k = 0; accept[k]; k++)
+	{
+	if (*s == accept[k])
+		return (s);
+	}
+	s++;
+	}
+	return ('\0');
+}
+
