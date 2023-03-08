a573c02effbde169f1dbcefab240b0ff5bf017f2
Orateng <1818.blackmail@gmail.com>
2023-03-08 06:10:22 -0800

    My return lenght of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strlen_recursion - Returns the length of a string
  * @s: The string to be measured
- * Return: The lenght of the string
+ *
+ * Return: The length of the string
  */
+
 int _strlen_recursion(char *s)
 {
 	int len = 0;
@@ -15,4 +16,5 @@ int _strlen_recursion(char *s)
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
+	return (len);
 }
