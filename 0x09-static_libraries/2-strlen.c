8c46136b6ce30fb9373e9359f9da9519977440a8
Orateng <1818.blackmail@gmail.com>
2023-03-13 16:59:51 +0000

    My static library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function to get the length of a string
+ * Description: get length of a string
+ * @*s: string length
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
