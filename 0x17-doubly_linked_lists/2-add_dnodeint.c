52199648267acdf896bf865c721940bd1d132242
Orateng <1818.blackmail@gmail.com>
2023-06-15 14:55:08 -0700

    My file

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *add_dnodeint - function that prints elements in reverse
+ *@head: Nodes starting point
+ *@n: The value of the node list
+ *Return: Null if new element is empty
+ */
+
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		exit(1);
+		return (0);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (0);
+}
