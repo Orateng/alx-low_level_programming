82869d4b68b5c0544e2a679c70cf398f46844544
Orateng <1818.blackmail@gmail.com>
2023-06-15 15:20:55 -0700

    My file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,6 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 	{
 		exit(1);
@@ -24,5 +23,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = new_node;
 	}
 	*head = new_node;
-	return (0);
+	return (new_node);
 }
