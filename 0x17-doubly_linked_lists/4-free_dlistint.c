c9b08c2c4c2d97365ac1892bd16182e31f450fb1
Orateng <1818.blackmail@gmail.com>
2023-06-15 18:09:25 -0700

    MY file

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,35 +1,24 @@
 #include "lists.h"
-/**
- *free_dlistint - function that frees a struct element in memory
- *@head: Nodes starting point
- *Return: new element
- */
 
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to the list.
+ **/
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *tmp = head;
-	int value = 0;
+	dlistint_t *actual_node;
+	dlistint_t *next_node;
 
-	if (new_node == NULL)
-		return;
-
-	new_node->n = value;
-	new_node->next = NULL;
-
-	if (head == NULL)
+	if (head)
 	{
-		head = new_node;
-		new_node->prev = NULL;
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(actual_node);
+			actual_node = next_node;
+			next_node = next_node->next;
+		}
+		free(actual_node);
 	}
-
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-		free(tmp->prev);
-	}
-	tmp->next = new_node;
-	new_node->prev = tmp;
-	free(tmp);
-
 }
