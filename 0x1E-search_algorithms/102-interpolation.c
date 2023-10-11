c662b4483bbc2e132ab7773edeb4f7f1321a4712
Orateng <1818.blackmail@gmail.com>
2023-10-11 09:41:43 -0700

    My file

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,7 +20,7 @@ int interpolation_search(int *array, size_t size, int value)
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		else
 		{
-			printf("Valiue checked array[%ld] is out of range\n", i);
+			printf("Value checked array[%ld] is out of range\n", i);
 			break;
 		}
 
